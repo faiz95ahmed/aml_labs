8fbb588d371e9329b8da3ec8e39f167f576706a9
Faiz <faiza@deepzen.io>
2021-02-27 01:52:22 +0000

    practicapractical 3 done

---
+++ prac3/prac3_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+conda remove -y --name aml_prac3 --all
+#conda create -y --name aml_prac3
+#conda activate aml_prac3
+#conda install -y matplotlib jupyter
+#conda install -y pytorch torchvision torchaudio cpuonly -c pytorch
+#yes | python3 -m pip install pyro-ppl
